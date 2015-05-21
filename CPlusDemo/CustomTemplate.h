282671236bd5be0547a37bbdca767db48bd231d6
xugang <xugangfm@163.com>
2015-05-21 14:53:42 +0800

    cqiptr

--- CPlusDemo/CustomTemplate.h
+++ CPlusDemo/CustomTemplate.h
@@ -11,69 +11,99 @@
 #include <iostream>
 
 //模板函数可以看做安全的宏定义，不能把申明和定义分开
-template <class TT>
-TT MyMax(TT a,TT b) {
-    if (a<b) {
-        return b;
-    }else
-    {
-        return a;
-    }
+template <class TT> TT MyMax(TT a, TT b) {
+  if (a < b) {
+    return b;
+  } else {
+    return a;
+  }
 }
 
-template <typename TTT>
-TTT MyMin(TTT a,TTT b) {
-    if (a<b) {
-        return a;
-    }else
-    {
-        return b;
-    }
+template <typename TTT> TTT MyMin(TTT a, TTT b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
 }
 
-template<typename T1,typename T2>
+template <typename T1, typename T2>
 
-class myClass{
-    
+class myClass {
+
 private:
-    
-    T1 I;
-    
-    T2 J;
-    
+  T1 I;
+
+  T2 J;
+
 public:
-    
-    myClass(T1 a, T2 b);//Constructor
-    
-    void show();
-    
+  myClass(T1 a, T2 b); // Constructor
+
+  void show();
 };
 
 //这是构造函数
 
 //注意这些格式
 
-template <typename T1,typename T2>
+template <typename T1, typename T2>
 
-//myClass<T1,T2>::myClass(T1 a,T2 b):I(a),J(b){}
+// myClass<T1,T2>::myClass(T1 a,T2 b):I(a),J(b){}
 
-myClass<T1,T2>::myClass(T1 a,T2 b){
-    this->I=a;
-    this->J=b;
+myClass<T1, T2>::myClass(T1 a, T2 b) {
+  this->I = a;
+  this->J = b;
 }
 
 //这是void show();
 
-template <typename T1,typename T2>
+template <typename T1, typename T2>
 
-void myClass<T1,T2>::show()
+void myClass<T1, T2>::show()
 
 {
-    
-    std::cout<<"I="<<I<<", J="<<J<<std::endl;
-    
+
+  std::cout << "I=" << I << ", J=" << J << std::endl;
 }
 
+/*********************************************/
 
+typedef enum { QI_MALLOC, QI_NEW } QIPTRMALLOCTYPE;
+
+template <class T, QIPTRMALLOCTYPE ptrmalloctype = QI_MALLOC> class CQIPtr {
+public:
+  CQIPtr(T *p = 0) { m_p = p; }
+
+  operator T *() { return m_p; }
+
+  T &operator*() const { return *m_p; }
+
+  T **operator&() { return &m_p; }
+
+  bool operator!() const { return (m_p == NULL); }
+
+  T *operator->() const { return m_p; }
+
+  T *operator=(T *p) {
+    Free();
+    m_p = p;
+    return m_p;
+  }
+
+  void Free() {
+    if (m_p) {
+      if (ptrmalloctype == QI_NEW)
+        delete (m_p);
+      else
+        free(m_p);
+    }
+    m_p = 0;
+  }
+
+  ~CQIPtr() { Free(); }
+
+private:
+  T *m_p;
+};
 
 #endif
--- CPlusDemo/main.cpp
+++ CPlusDemo/main.cpp
@@ -10,9 +10,12 @@
 #include "Rect.h"
 #include "CustomTemplate.h"
 
-//template <class TT>
+using namespace std;
+using namespace custom;
+
+// template <class TT>
 //
-//TT MyMax(TT a,TT b) {
+// TT MyMax(TT a,TT b) {
 //    if (a<b) {
 //        return b;
 //    }else
@@ -23,54 +26,65 @@
 
 class A {
 public:
-    A(){std::cout << "create A\n";};
-     ~A(){ std::cout << "delete A\n"; }
+  A() { std::cout << "create A\n"; };
+  ~A() { std::cout << "delete A\n"; }
 };
 
-class B :public A{
+class B : public A {
 public:
-    B(){std::cout << "create B\n";};
-    ~B(){ std::cout << "delete B\n"; }
+  B() { std::cout << "create B\n"; };
+  ~B() { std::cout << "delete B\n"; }
 };
 
+void TestName_Space(void) {
+  // xx= 190;
+  // insert code here...
+  std::cout << "Hello, World!\n";
 
-void TestName_Space(void)
-{
-    // xx= 190;
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    
-    custom::Rect *frame = new custom::Rect();
-    
-    frame->x=xx;
-    frame->y=custom::yy;
-    
-    frame->printo();
+  custom::Rect *frame = new custom::Rect();
+
+  frame->x = xx;
+  frame->y = custom::yy;
+
+  frame->printo();
 }
-void TestTemplate(){
-    
-    int ret = MyMax(5, 8);
-    
-    printf("max ==>%d\n",ret);
-    
-    
-    char a= MyMax('A', 'B');
-    printf("max ==>%c\n",a);
+void TestTemplate() {
+
+  int ret = MyMax(5, 8);
+
+  printf("max ==>%d\n", ret);
+
+  char a = MyMax('A', 'B');
+  printf("max ==>%c\n", a);
 }
 
-int main(int argc, const char * argv[])
-{
+void TestCQI() {
 
-    //TestName_Space();
-    //TestTemplate();
-    
-    //myClass<int, int> *obj = new myClass<int, int>(5,9);
-    //obj->show();
-    
-    A *a =new B();
-    delete a;
-    
-    
-    return 0;
+  Rect *frame = new Rect();
+
+  CQIPtr<Rect, QI_NEW> obj(frame);
+
+  obj->printo();
+  // obj.Free();
+  // delete obj;
 }
 
+int main(int argc, const char *argv[]) {
+
+  // TestName_Space();
+  // TestTemplate();
+
+  // myClass<int, int> *obj = new myClass<int, int>(5, 9);
+
+  // myClass<string,string> *obj = new myClass<string,string>("dwefde",
+  // "42343");
+
+  // obj->show();
+
+  // A *a =new B();
+  // delete a;
+
+  TestCQI();
+
+  return 0;
+}
