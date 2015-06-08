6526edda04e7528ee8371949a878eff2432a2f3e
xugang <xugangfm@163.com>
2015-06-08 20:16:38 +0800

    cpp

--- CPlusDemo/main.cpp
+++ CPlusDemo/main.cpp
@@ -9,6 +9,14 @@
 #include <iostream>
 #include "Rect.h"
 #include "CustomTemplate.h"
+#include <memory>
+#include <string>
+#include <functional>
+#include <vector>
+#include <set>
+#include <mutex>
+#include <future>
+#include <list>
 
 using namespace std;
 using namespace custom;
@@ -23,6 +31,7 @@ using namespace custom;
 //        return a;
 //    }
 //}
+std::unique_ptr<Rect> frame ;
 
 class A {
 public:
@@ -68,6 +77,71 @@ void TestCQI() {
   // obj.Free();
   // delete obj;
 }
+unique_ptr<int> Func(unique_ptr<int> a)
+
+{
+    
+    cout<<(*a)++<<endl;
+    
+    return a;
+    
+}
+void TestPTR(){
+    
+    //Rect *frame = new Rect();
+    
+    frame= std::make_unique<Rect>();
+    frame->w = 100;
+    frame->printo();
+    
+    std::unique_ptr<Rect> frame0 = std::move(frame);
+    frame0->printo();
+    
+    frame0.reset();// 显示释放内存
+    
+    std::shared_ptr<Rect> frame1 = std::make_shared<Rect>();
+    
+    frame1->x = 10;
+    frame1->y = 11;
+    
+    
+    
+    //std::shared_ptr<Rect> frame1_1 = std::move(frame1);
+    std::shared_ptr<Rect> frame1_1 = frame1;
+    frame1_1->printo();
+    frame1->printo();
+    
+    
+
+    //weak_freme.printo();
+    
+    
+    std::cout << "\n====\n" << std::endl;
+    
+    std::unique_ptr<int> number = std::make_unique<int>(10);
+    std::shared_ptr<int> number2(new int(12));
+    std::shared_ptr<int> number3(new int(13));
+    std::weak_ptr<int> weak_number = number2;
+    //std::unique_ptr<int> result = Func(move(number));
+    number = Func(move(number));;
+    std::cout << *number << std::endl;
+    
+    std::cout << "\n====\n" << std::endl;
+    
+    auto _number = Func(move(number));;
+    std::cout << *_number << std::endl;
+    std::cout << "\n====\n" << std::endl;
+    
+    
+    number3.swap(number2);
+    
+    std::cout << "number2="<< *number2 << std::endl;
+    std::cout << "number3="<< *number3 << std::endl;
+    
+}
+
+
+
 
 int main(int argc, const char *argv[]) {
 
@@ -84,7 +158,8 @@ int main(int argc, const char *argv[]) {
   // A *a =new B();
   // delete a;
 
-  TestCQI();
+  //TestCQI();
+    TestPTR();
 
   return 0;
 }
